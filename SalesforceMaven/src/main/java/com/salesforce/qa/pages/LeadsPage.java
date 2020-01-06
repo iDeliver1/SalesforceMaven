fa5575d24ec89b17b844f1b8a7d77ed8e51ea2af
iDeliver1 <sourabhc@ideliver-inc.com>
2020-01-06 19:22:41 +0530

    update lib files

--- SalesforceMaven/src/main/java/com/salesforce/qa/pages/LeadsPage.java
+++ SalesforceMaven/src/main/java/com/salesforce/qa/pages/LeadsPage.java
@@ -1,5 +1,107 @@
 package com.salesforce.qa.pages;
 
-public class LeadsPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com.salesforce.qa.base.TestBase;
+
+public class LeadsPage extends TestBase {
+	@FindBy(xpath="//li[@id='Lead_Tab']")
+	WebElement LeadTab ;
+	
+	@FindBy(xpath="//a[@id='tryLexDialogX']")
+	WebElement DialogeClose;
+	
+	@FindBy(xpath="//input[@name='new']")
+	WebElement NewButton;
+	
+	@FindBy(xpath="//input[@id='name_firstlea2']")
+	WebElement FName ;
+	
+	@FindBy(xpath="//input[@id='name_lastlea2']")
+	WebElement LName;
+	
+	@FindBy(xpath="//input[@id='lea20']")
+	WebElement Campaign;
+	
+	@FindBy(xpath="//input[@id='lea3']")
+	WebElement CompanyName;
+	
+	@FindBy(xpath="//select[@id='lea13']")
+	WebElement Status 	;
+	
+	@FindBy(xpath="//td[@id='bottomButtonRow']/input[@title='Save']")
+	WebElement Save;
+	
+	@FindBy(xpath="//div[@class='textBlock']/h2")
+	WebElement HeadName;
+	
+	
+	public LeadsPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String validateLeadsPageTitle(){
+		return driver.getTitle();
+	}
+		
+	public void CreateLeads(String FNName, String LNName, String CompanyNName, String SStatus, String CCampaign){
+		//try
+		//{
+			//LeadTab.click();
+				try
+				{
+				
+					//Close the popup window
+					DialogeClose.click();
+				}
+				catch(Exception f) {}
+				NewButton.click();
+				try
+				{
+					FName.isDisplayed();
+					//-----------------------------Reporter
+					//Utility_Object.fReportpass("Create Lead", "Create Lead page is open successfully", logger, driver);
+					//------------------------------------
+				}
+				catch(Exception e)
+				{
+					//-----------------------------Reporter
+					//Utility_Object.fReportfail("Create Lead", "Error :" + e +" Create Lead page is not open successfully", logger, driver, Extndreport);
+					//------------------------------------
+				}
+				FName.sendKeys(FNName);
+				LName.sendKeys(LNName);
+				Campaign.sendKeys(CCampaign);
+				CompanyName.sendKeys(CompanyNName);
+			Select Status1_ = new Select(  Status);
+			Status1_.selectByValue(SStatus);
+			Save.click();
+			//WebDriverWait wait = new WebDriverWait(driver,6);
+			//wait.until(ExpectedConditions.presenceOfElementLocated((By) HeadName));
+			String LeadName = HeadName.getText();
+				if(LeadName.contains(FNName.concat(" " + LNName)))
+				{
+					//-----------------------------Reporter
+					//Utility_Object.fReportpass("Lead create", "Lead is successfully created", logger, driver);
+					//------------------------------------
+				}
+				else
+				{
+					
+					//-----------------------------Reporter
+					//Utility_Object.fReportfail("Lead create", "Lead is not successfully created", logger, driver, Extndreport);
+					//------------------------------------
+				}
+		}
+		//catch(Exception E)
+		//{
+			//-----------------------------Reporter
+			//System.out.println(E);
+			//------------------------------------
+		//}
+	//}
 
 }
--- SalesforceMaven/src/test/java/com/salesforce/qa/testcases/LoginPageTest.java
+++ SalesforceMaven/src/test/java/com/salesforce/qa/testcases/LoginPageTest.java
@@ -1,47 +1,82 @@
 package com.salesforce.qa.testcases;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.salesforce.qa.pages.HomePage;
+import com.salesforce.qa.pages.LeadsPage;
 import com.salesforce.qa.pages.LoginPage;
+import com.salesforce.qa.util.Utility_Libraries;
 
 import org.testng.*;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.salesforce.qa.base.TestBase;
 
 public class LoginPageTest extends TestBase{
+	
+	ExtentReports extent;
+	ExtentTest logger;
 	LoginPage loginPage;
 	HomePage homepage;
+	LeadsPage leadspage;
+	Utility_Libraries utilityobject = new Utility_Libraries();
 	
 	public LoginPageTest(){
 		super();
 	}
 	
-	@BeforeMethod
+	@BeforeTest
 	public void setUp(){
 		initialization();
+		extent = utilityobject.fReport();
 		loginPage = new LoginPage();
-		//homepage = new HomePage();
-		HomePage homepage;
-		
 	}
 	
-	@Test
-	//@DataProvider[]
+	@Test(priority=1)
 	public void LoginTest(){
 		
-		Assert.assertEquals(true, loginPage.validateSalesForceLogo());
-		
-		homepage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
-		
-		//	homepage.validateHomePageTitle();
-		//home = homepage.validateHomePageTitle();
-		Assert.assertEquals("Home Page ~ Salesforce - Developer Edition", homepage.validateHomePageTitle());
+		logger = extent.createTest("Login");
+		logger.log(Status.PASS, "Login ");
+		homepage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));		
+			if(homepage.validateHomePageTitle().contains("Home Page ~ Salesforce - Developer Edition"))
+			{
+				logger.log(Status.PASS, "Login Succesfull Home page is open "+homepage.validateHomePageTitle());
+			}
+			else
+			{
+				logger.log(Status.FAIL, "Login Succesfull Home page is not open ");
+			}
 	}
 	
 	
+	@Test(priority=2)
+	public void LeadTest(){
+			
+		logger = extent.createTest("CreateLeads");
+		logger.log(Status.PASS, "CreateLeads");
+		leadspage = homepage.clickOnLeadsLink();
+			if(leadspage.validateLeadsPageTitle().contains("Leads: Home ~ Salesforce - Developer Edition"))
+			{
+				logger.log(Status.PASS, "Leads page is open "+ leadspage.validateLeadsPageTitle());
+			}
+			else
+			{
+				logger.log(Status.FAIL, "Leads page is not open ");
+			}
+		leadspage.CreateLeads("Aviraj", "Lall", "iDeliver", "Open - Not Contacted", "Lall");		
+	}
+	
+	@AfterMethod
+	public void Flush()
+	{
+		extent.flush();
+	}
 
 }
